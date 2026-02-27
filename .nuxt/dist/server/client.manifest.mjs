2852ecb1855051a457c21d8a11c96245cbb00ee7
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2026-02-27 17:30:04 +0100

    chore: add .nuxt and dist to .gitignore

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///Users/enricoreinsdorf/Projekte/SnackEase/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
